709ea89fc7ade4c62668b75d22f085d71b810c25
JeeNi <parkej9494@gmail.com>
2020-02-11 16:36:51 +0900

    최대값의 위치를 구하는 알고리즘

--- study_8day.py
+++ study_8day.py
@@ -12,3 +12,19 @@ def find_max(a):
 
 v = [17, 92, 18, 33, 58, 7, 33, 42]
 print(find_max(v))
+
+
+# 최댓값의 위치를 구하는 알고리즘
+# 입력: 숫자가 n개 들어 있는 리스트
+# 출력: 숫자 n개 중에서 최댓값이 있는 위치(0부터 n-1까지의 값)
+
+def find_max_idx(a):
+    n = len(a)
+    max_idx = 0
+    for i in range(1, n):
+        if a[i] > a[max_idx]:
+            max_idx = i
+    return max_idx
+
+v = [17, 92, 18, 33, 58, 7, 33, 42]
+print(find_max_idx(v))
